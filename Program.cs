9860b7e35e4a03d31b05ccf337d4690e409f445a
Renato <remarks01@gmail.com>
2020-01-09 22:08:36 -0300

    Primeiro commit - Programa exemplo com C#

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno [] alunos  = new Aluno [5];
+            int indiceAluno = 0;
+            
+            string opcaoUsuario = obterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o nome do Aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException("A nota do aluno dever ser em decimal!");
+                        }  
+                        alunos [indiceAluno] = aluno;
+                        indiceAluno++;
+                        Console.WriteLine();
+
+                        break;
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if(!string.IsNullOrEmpty(a.Nome))
+                            {
+                                 Console.WriteLine($"ALUNO: {a.Nome} - NOTA; {a.Nota}");
+                            }   
+                        }
+                                                    
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for( int i = 0; i < alunos.Length; i++)
+                        {
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Console.WriteLine($"MEDIA GERAL: {mediaGeral}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }                
+                opcaoUsuario = obterOpcaoUsuario();
+            }
+        }
+
+        private static string obterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opcao desejada");
+            Console.WriteLine("1 - Adicionar novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular media geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            String opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
